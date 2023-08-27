e6b2e2678ed162c20bd28131cdee063f53275bf2
Chris Norman <chris.norman2@googlemail.com>
2023-08-27 18:16:02 +0100

    Removed subcommands.

--- bin/server.dart
+++ bin/server.dart
@@ -1,37 +1,156 @@
-// ignore_for_file: avoid_print
 import 'dart:io';
 
-import 'package:args/command_runner.dart';
+import 'package:args/args.dart';
+import 'package:database/database.dart';
 import 'package:logging/logging.dart';
-import 'package:server/src/app_config.dart';
-import 'package:server/src/server_commands/profiles_server_command.dart';
-import 'package:server/src/server_commands/run_server_command.dart';
+import 'package:logging_appenders/logging_appenders.dart';
+import 'package:shelf_plus/shelf_plus.dart';
 
 Future<void> main(final List<String> arguments) async {
-  Logger.root
-    ..level = Level.ALL
-    ..onRecord.listen(print);
-  final runner = CommandRunner(
-    Platform.executable,
-    'The Monolith game server.',
-  );
-  runner.argParser
+  Logger.root.level = Level.ALL;
+  final argParser = ArgParser(allowTrailingOptions: false)
+    ..addSeparator('Database')
     ..addOption(
       'database',
       abbr: 'd',
       defaultsTo: 'db.sqlite3',
-      help: 'The file to load the database from',
+      help: 'The filename to load the database from.',
     )
-    ..addFlag('log-sql', help: 'Whether SQL statements should be logged');
+    ..addFlag('log-sql', help: 'Log SQL statements.')
+    ..addSeparator('Server Profiles')
+    ..addOption(
+      'profile-name',
+      abbr: 'p',
+      defaultsTo: '',
+      help: 'The name of the profile to load.',
+    )
+    ..addSeparator('Logging')
+    ..addOption(
+      'log-file',
+      abbr: 'l',
+      defaultsTo: '-',
+      help: 'The log file to use.',
+      allowedHelp: {
+        '-': 'Log to stdout.',
+        '<filename>': 'Use <filename> as the log file.',
+      },
+    )
+    ..addSeparator('Getting Help')
+    ..addFlag('help', abbr: 'h', help: 'Show help.');
+  final ArgResults argResults;
   try {
-    final argResults = runner.parse(arguments);
-    final appConfig = AppConfig.fromArgResults(argResults);
-    runner
-      ..addCommand(RunServerCommand(appConfig))
-      ..addCommand(ProfilesServerCommand(appConfig));
-    await runner.run(arguments);
-  } on UsageException catch (e) {
-    print(e);
-    exit(64);
+    argResults = argParser.parse(arguments);
+  } on ArgParserException catch (e) {
+    stderr.writeln(e);
+    exit(127);
+  }
+  if (argResults['help'] as bool) {
+    stdout
+      ..writeln('Monolith game server')
+      ..writeln(argParser.usage);
+    exit(0);
+  }
+  final databaseFile = argResults['database'] as String;
+  final logSql = argResults['log-sql'] as bool;
+  final profileName = argResults['profile-name'] as String;
+  final database = MonolithDatabase(
+    file: File(databaseFile),
+    logStatements: logSql,
+  );
+  RotatingFileAppender? fileAppender;
+  try {
+    final logFilename = argResults['log-file'] as String;
+    if (logFilename == '-') {
+      PrintAppender.setupLogging();
+    } else {
+      fileAppender = RotatingFileAppender(baseFilePath: logFilename)
+        ..attachToLogger(Logger.root);
+    }
+    final serverLogger = Logger('Server');
+    final serverProfilesDao = database.serverProfilesDao;
+    final profiles =
+        (await serverProfilesDao.getServerProfileContexts()).toList();
+    if (profiles.isEmpty) {
+      final serverSecurityContextsDao = database.serverSecurityContextsDao;
+      final serverSecurityContext =
+          await serverSecurityContextsDao.createServerSecurityContext(
+        chainFile: File('fullchain.pem'),
+        keyFile: File('privkey.pem'),
+      );
+      final insecure = await serverProfilesDao.createServerProfile(
+        name: 'Insecure',
+        host: '0.0.0.0',
+        port: 8080,
+      );
+      final secure = await serverProfilesDao.createServerProfile(
+        name: 'Secure',
+        host: '0.0.0.0',
+        port: 8080,
+        securityContext: serverSecurityContext,
+      );
+      profiles.addAll([
+        ServerProfileContext(serverProfile: insecure),
+        ServerProfileContext(serverProfile: secure),
+      ]);
+    }
+    final ServerProfileContext profileContext;
+    if (profileName.isEmpty) {
+      profileContext = profiles.first;
+    } else {
+      try {
+        profileContext = profiles.firstWhere(
+          (final element) => element.serverProfile.name
+              .toLowerCase()
+              .startsWith(profileName.toLowerCase()),
+        );
+        // ignore: avoid_catching_errors
+      } on StateError {
+        stderr
+          ..writeln('No server profile found matching $profileName.')
+          ..writeln('Profiles:');
+        for (final profile in profiles) {
+          stderr.writeln(profile.serverProfile.name);
+        }
+        exit(126);
+      }
+    }
+    final serverProfile = profileContext.serverProfile;
+    final host = serverProfile.host;
+    final port = serverProfile.port;
+    final serverSecurity = profileContext.securityContext;
+    final SecurityContext? securityContext;
+    if (serverSecurity == null) {
+      securityContext = null;
+    } else {
+      securityContext = SecurityContext()
+        ..useCertificateChain(
+          serverSecurity.chainFilePath,
+          password: serverSecurity.chainPassword,
+        )
+        ..usePrivateKey(
+          serverSecurity.keyFilePath,
+          password: serverSecurity.keyPassword,
+        );
+    }
+    final protocol = securityContext == null ? 'http' : 'https';
+    await shelfRun(
+      () {
+        final router = Router().plus
+          ..use(logRequests())
+          ..get('/', () => 'Welcome to Monolith.');
+        return router;
+      },
+      defaultBindAddress: host,
+      defaultBindPort: port,
+      defaultEnableHotReload: false,
+      onClosed: () => serverLogger.fine('Server done.'),
+      onStarted: (final address, final port) => serverLogger.fine(
+        'Listening on $protocol://$address:$port.',
+      ),
+      securityContext: securityContext,
+    );
+  } finally {
+    await database.close();
+    await fileAppender?.dispose();
   }
 }
